f92bc18a3727c9703d1595a99495ebae4c559fc2
DeAsia7 <asiamallory277@gmail.com>
2025-05-01 21:48:43 -0400

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.arguments(express.json());
+app.use(express.json());
 
 let cars =[
  { id: 1, brand: "Toyota", model: "Corolla", year: 2020, color: "Red" },
