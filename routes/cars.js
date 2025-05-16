6d18ebb7412b99929500f855a9b05364aab80e55
DeAsia7 <asiamallory277@gmail.com>
2025-05-15 21:25:03 -0400

    2124

--- routes/cars.js
+++ routes/cars.js
@@ -11,14 +11,14 @@ router.get('/after-2020', async (req, res) => {
 })
 
 //change price of a car
-router.put('/update-price/:id', async (req, res) => {
+router.put('/update-price', async (req, res) => {
    const result =  await db.update(cars).set({price: 400}).where(eq(cars.id, 1));
     res.json({message: 'Price updated successfully', result});
 })
 
 
 //remove a car
-router.delete('/delete/:id', async (req, res) => {
+router.delete('/delete', async (req, res) => {
    const result =  await db.delete(cars).where(lte(cars.year, 2008));
 
     res.json({ message: 'Car deleted successfully', result });
@@ -36,7 +36,7 @@ await db.insert(cars).values([
 })
 
 //cars tht are red and start with m
-router.get('/red-m', async (req, res) => {
+router.get('/red-m%', async (req, res) => {
     const result = await db.select().from(cars).where(and(eq(cars.color, 'red'), like(cars.make, 'm%')));
     res.json(result);
 })
@@ -49,6 +49,6 @@ router.get('/red-m', async (req, res) => {
 GET - Fetch data app.get('/items', handler)
 POST -  Add data app.post('/items', handler)
 PUT - Update app.put('/items/:id', handler)
-Patch - chan
+Patch - changes specific data app.patch('/items/:id', handler)
 DELETE - Remove app.delete('/items/:id', handler)
 */
