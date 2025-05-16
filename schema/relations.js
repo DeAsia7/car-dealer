7508ed3156ee0824026b5f56ad99dd40a9cf8038
DeAsia7 <asiamallory277@gmail.com>
2025-05-15 20:46:40 -0400

    2046

--- schema/relations.js
+++ schema/relations.js
@@ -1,7 +1,23 @@
 import {relations} from 'drizzle-orm';
 import {cars, clients, sales} from './schema.js';
 
-//cars-> sales
+//cars -> sales
 export const carRelations = relations(cars, ({many}) => ({
     sales: many(sales),
 })); 
+
+//clients -> sales
+export const clientRelations = relations(clients, ({many}) => ({
+    sales: many(sales),
+}));
+// sale -> client and car
+export const salesRelations = relations(sales, ({one}) => ({
+    car: one(cars, {
+        fields: [sales.car_id],
+        references: [cars.id],
+    }),
+    client: one(clients, {
+        fields: [sales.client_id],
+        references: [clients.id],
+    }),
+}));
