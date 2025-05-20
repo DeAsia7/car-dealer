23b0e2b33b91ab6ea7dcc95c26c6083fb86229fd
DeAsia7 <asiamallory277@gmail.com>
2025-05-19 21:10:23 -0400

    2110 new

--- index2.js
+++ index2.js
@@ -2,13 +2,21 @@ import express from 'express';
 import carRoutes from './routes/cars.js';
 import clientRoutes from './routes/clients.js';
 import salesRoutes from './routes/sales.js';
+import authRoutes from './routes/auth.JS';
+import {logger} from './middleware/logger.js';
+import {verifyToken } from './middleware/token.js';
 
 const app = express();
 const PORT = 3000
 
-app.use(carRoutes)
-app.use(clientRoutes)
-app.use(salesRoutes)
+app.use(express.json());
+app.use(logger);
+
+app.use(authRoutes);
+
+app.use(carRoutes, verifyToken, '/cars')
+app.use(clientRoutes, verifyToken, '/')
+app.use(salesRoutes, verifyToken, '/')
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
---
+++ middlewares/token.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+export function verifyToken(req, res, next) {
+    const authHeader = req.header['authorization'];
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({message: 'Missing or invalid token'});
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env('JWT_SECRET'));
+
+    }
+    
+}
