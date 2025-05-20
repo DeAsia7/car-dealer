afcd0850d0bf2e5744e0585dcc33ab9656059c8a
DeAsia7 <asiamallory277@gmail.com>
2025-05-19 22:01:47 -0400

    2201

--- middlewares/token.js
+++ middlewares/token.js
@@ -11,9 +11,9 @@ export function verifyToken(req, res, next) {
         req.user = decoded;
         next();
     }catch{
-        
+        return res.status(401).json({message: 'Invalid token'});
+
     }
 
     }
     
-}
