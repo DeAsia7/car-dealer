e994a05a593c32b6b39139ad255a20499f2177b0
DeAsia7 <asiamallory277@gmail.com>
2025-05-29 00:36:53 -0400

    fiixng

--- validators/index.js
+++ validators/index.js
@@ -1,3 +1,42 @@
+import express from 'express';
+import carRoutes from '../routes/cars.js';
+import clientRoutes from '../routes/clients.js';
+import salesRoutes from '../routes/sales.js';
+import authRoutes from '../routes/auth.js';
+import {logger} from '../middlewares/logger.js';
+import {verifyToken } from '../middlewares/token.js';
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+const PORT = 3000
+
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true 
+}));
+app.use(express.json());
+app.use(logger);
+
+app.use(authRoutes);
+
+app.use('/cars', verifyToken, carRoutes);
+app.use('/' , verifyToken, clientRoutes);
+app.use('/', verifyToken, salesRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+
+
+
+
+
+
+/*
+
 import { z } from 'zod';
 
 //building schemsd for each body 
@@ -23,3 +62,4 @@ export const salesSchema = z.object({
     car_id: z.number().int(), 
 });
  
+*/
