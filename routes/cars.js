74830d17d5d00383d0d3a703032c30497011fec4
DeAsia7 <asiamallory277@gmail.com>
2025-06-04 13:38:26 -0400

    fixing

--- routes/cars.js
+++ routes/cars.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // list cars after 2020
 router.get('/after-2020', async (req, res) => {
-    const result = await db.select().from(cars).where(gt(cars.year, Number(2020)));
+    const result = await db.select().from(cars).where(gt(cars.year,2020));
     res.json(result);
 })
 
