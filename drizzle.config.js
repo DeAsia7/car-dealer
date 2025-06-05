c5439621dbd2683f69d4faddbfb3f1654e56d29a
DeAsia7 <asiamallory277@gmail.com>
2025-06-05 16:14:44 -0400

    updating

--- drizzle.config.js
+++ drizzle.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_default,
+        database: process.env.DB_DEFAULT,
         port: process.env.DB_PORT,
 
     }
