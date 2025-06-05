e567489d4891046e616538781c9cd9e432017b72
DeAsia7 <asiamallory277@gmail.com>
2025-06-05 18:37:10 -0400

    fix

--- middlewares/token.js
+++ middlewares/token.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export function verifyToken(req, res, next) {
     const authHeader = req.headers['authorization'];
-    console.log(authHeader)
+
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({message: 'Missing or invalid token'});
     }
